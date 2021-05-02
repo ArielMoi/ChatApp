d3fab1c049f91677cfeb495052408f3361bb43fc
Ariel Moise <ariel.m23@gmail.com>
2021-05-02 16:47:13 +0300

    additions

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,59 @@
 const socket = io();
 
-document.querySelector(".message-from").addEventListener("submit", (e) => {
-  event.preventDefault();
-  const msg = e.target.elements.message.value;
-  socket.emit("message", msg);
-});
+const messagesElement = document.querySelector("#messages");
+
+// templated
+const messageTemplate = document.querySelector("#message-template").innerHTML; // to get the html data and not script
 
 socket.on("message", (msg) => {
   console.log(msg);
+
+  const html = Mustache.render(messageTemplate, {
+    message: msg.text,
+    createdAt: moment(msg.createdAt).format('h:mm a')
+  });
+
+  messagesElement.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("location", (location) => {
+  console.log(location);
+
+  const html = Mustache.render(
+    document.querySelector("#location-template").innerHTML,
+    {
+      location: location.location,
+      createdAt: moment(location.createdAt).format("h:mm a"),
+    }
+  );
+
+  messagesElement.insertAdjacentHTML("beforeend", html);
+});
+
+document.querySelector(".message-from").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const msg = e.target.elements.message.value;
+  socket.emit("message", msg, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("message delivered");
+  });
+});
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    // check if cursor have geolocation options
+    return alert("geolocation isn`t supported");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      `http://google.com/maps/?q=${position.coords.latitude},${position.coords.longitude}`,
+      (error) => {
+        console.log("location delivered");
+      }
+    );
+  });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
 const express = require("express");
+const Filter = require('bad-words')
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app); // creating the server outside of the express library to later connect it to socket io
@@ -12,21 +17,30 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on("connection", (socket) => {// socket is an object that contains info on the connection
   console.log("new webSocket connection");
 
-  socket.emit("message", "hello world");
-  socket.broadcast.emit('message', 'a new user joined')
+  socket.emit("message", generateMessage("generateMessage")); // socket emit to current user
+  socket.broadcast.emit("message", generateMessage("a new user joined"));
 
-  socket.on("message", (msg) => {
-    io.emit("message", msg); // io emit to every connection (except current)
+  socket.on("message", (msg, callback) => {
+    const filter = new Filter();
+
+    if(filter.isProfane(msg)){
+        return callback('bad words')
+    }
+
+    io.emit("message", generateMessage(msg)); // io emit to all connected clients
+    callback();
+  });
+
+  socket.on("sendLocation", (location, callback) => {
+    io.emit("location", generateLocationMessage(location));
+    callback();
   });
 
   socket.on('disconnect', () => {
-      io.emit('message', 'user has left')
-      
+    io.emit("message", "user has left");
   })
 });
 
